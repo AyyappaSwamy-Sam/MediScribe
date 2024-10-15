278ecfec023a9ef1f9a70f2026dd6e5c7047cbe5
subhash137 <subhash6300101199@gmail.com>
2024-10-15 14:56:48 +0530

    cancel appointments added

--- server/src/routes/doctorPageRoutes.js
+++ server/src/routes/doctorPageRoutes.js
@@ -8,6 +8,7 @@ router.get('/doctor/recent', appointmentController.getRecentAppointments);
 router.get('/doctor/todayappointments', appointmentController.getTodayAppointments);
 router.get('/doctor/next', appointmentController.getNextAppointment);
 router.get('/doctor/:doctorId', appointmentController.getDoctorAppointments);
-router.get('/doctors/my-profile/:doctorId', doctorController.getDoctorById)
+router.get('/doctors/my-profile/:doctorId', doctorController.getDoctorById);
+router.post('/doctor/UpdateData/:appointmentId', appointmentController.updateAppointment);
 
 module.exports = router;
